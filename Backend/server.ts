5e9bcece062d764884098fa3abbffaf7b8ca6f59
Aditya Raj <adityaraj34@icloud.com>
2026-07-08 01:46:36 +0530

    server

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,2 +1,12 @@
-var s:string = "hello world"
-console.log("hello rld")
+import express from 'express'
+import dotenv from 'dotenv'
+import authRoute from './src/routes/auth.route.js'
+
+dotenv.config();
+const app = express();
+app.use("api/auth",authRoute);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
